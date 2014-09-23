686f3eb69db282543f6dcaccb68c2148e89db2a3
lzheng <lzheng@XOS-BH1SDV1.xostech.internal>
2014-09-22 20:08:25 -0400

    add jsonFile parser

---
+++ python-convertor/src/main/java/org/easyerp/parser/JsonFileParser.java
@@ -0,0 +1,23 @@
+package org.easyerp.parser;
+
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class JsonFileParser {
+	static public void main(String[] args) throws IOException {
+		JSONLexer lexer = new JSONLexer(new ANTLRFileStream(JsonFileParser.class.getClassLoader().getResource("__openerp__.py").getFile()));
+		JSONParser parser = new JSONParser(new CommonTokenStream(lexer));
+		
+		print(parser.pair());	
+	}
+
+	private static void print(ParseTree tree) {
+		System.out.println("new line: " + tree.getText() + " : " + tree.getChildCount());
+		for(int i = 0; i < tree.getChildCount(); i ++) {
+			print(tree.getChild(i));
+		}
+	}
+}
